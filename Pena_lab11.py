41d3987e3e07adaf69058c6135eaa218fb89621b
CzarleneMikaelaPena <czarlenemikaelapena04@gmail.com>
2024-11-07 10:13:15 +0800

    Update Pena_lab11.py
    "fourth commit"

--- Pena_lab11.py
+++ Pena_lab11.py
@@ -1,6 +1,6 @@
 student_grades = []
 passed = 0
-valid_input = True
+grade_input = True
 
 while True:
     grade = input("Type student grade (type 'done' to finish): ")
@@ -12,14 +12,14 @@ while True:
     
     if grade < 40 or grade > 100:
         print("Invalid grade")
-        valid_input = False
+        grade_input = False
         break
     
     student_grades.append(grade)
     if grade >= 75:
         passed += 1
 
-if valid_input and student_grades:
+if grade_input and student_grades:
     average_grade = round(sum(student_grades) / len(student_grades), 2)
     passing_percentage = round(passed / len(student_grades) * 100, 2)
     
